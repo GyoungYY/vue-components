07921d4b687218249f115ee3b8bb5203ebe8b419
yangguang <yangguang@bilibili.com>
2017-11-10 17:17:23 +0800

    element ui

--- src/components/DetailList/ElementUI.vue
+++ src/components/DetailList/ElementUI.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
-        ElementUI详情
+    <div class="detail-container">
+        <h1>ElementUI</h1>
+        <h3>使用方法</h3>
+        <div class="use-method">
+            npm install element-ui --save-dev
+        </div>
+
+        <div>
+            <el-button type="primary" @click="goBack()">返回列表</el-button>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+            goBack:function () {
+                this.$router.push({path:'/List'});
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .detail-container{
+        padding:20px 80px;
+    }
+    h3 {
+        text-align: left;
+        padding:10px;
+        border-bottom: 1px solid #ddd;
+    }
+    .use-method{
+        margin: 20px 0;
+        padding:10px;
+        background-color: #f6f8fa;
+        text-align: left;
+    }
+</style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -4,18 +4,22 @@
             <h2>ElementUI</h2>
             <div>Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库</div>
         </div>
+
     </div>
 </template>
 
 <script>
     export default {
         data(){
+            return{
 
+            }
         },
         methods:{
             gotoDetail:function (path) {
                 this.$router.push({path:path});
-            }
+            },
+
         }
     }
 </script>
